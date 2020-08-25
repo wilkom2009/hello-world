0342068549145720afafefced4c3548f07db7ab0
Wilson <wilkom2009@yahoo.fr>
2020-08-25 22:31:05 +0000

    First commit

---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Header from "../components/header"
+
+export default function About() {
+  return (
+    <div style={{ color: `teal` }}>
+      <Header headerText="Error page" />
+      <p>404-Page not found!</p>
+    </div>
+  )
+}
