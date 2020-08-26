f6b25e028db4c69d909061ba990503c658ea729e
Wilson <wilkom2009@yahoo.fr>
2020-08-26 00:23:18 +0000

    Adding Typography plugin

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,13 @@
 import React from "react"
-import Header from "../components/header"
-import Container from "../components/container"
+import Layout from "../components/layout"
 
 export default function About() {
   return (
-    <Container>
-      <Header headerText="About Gatsby" />
-      <p>Such wow. Very React.</p>
-    </Container>
+    <Layout>
+      <h1>About me</h1>
+      <p>
+        I’m good enough, I’m smart enough, and gosh darn it, people like me!
+      </p>
+    </Layout>
   )
 }
